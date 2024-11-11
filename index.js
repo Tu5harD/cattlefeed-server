a88ff1ada5618fb7320e732605e32e7386f6c770
DevTusharDukane <tushardukane9@gmail.com>
2024-11-11 17:58:03 +0530

    change origin

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: ["http://localhost:5173", "https://tushartraders.shop"],
   })
 );
 
