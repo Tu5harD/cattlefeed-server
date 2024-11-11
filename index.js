6ce1d8d53f576cf40dfeca98aaadfc97435a9cbc
DevTusharDukane <tushardukane9@gmail.com>
2024-11-11 23:27:00 +0530

    remove dependencies

--- index.js
+++ index.js
@@ -5,33 +5,30 @@ const Product = require("./src/module/products");
 const Order = require("./src/module/orders");
 const cors = require("cors");
 const app = express();
-const cookieParser = require("cookie-parser");
 
-// const authRoutes = require("./src/routes/authRoutes");
 connectDB();
 
 app.use(express.json());
-app.use(cookieParser());
-const allowedOrigins = ["https://tushartraders.shop", "http://localhost:5173"];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // Check if the incoming origin is in the allowedOrigins array or if it’s undefined (for server-to-server requests)
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
+// const allowedOrigins = ["https://tushartraders.shop", "http://localhost:5173"];
 
 // app.use(
 //   cors({
-//     origin: "https://tushartraders.shop",
+//     origin: (origin, callback) => {
+//       // Check if the incoming origin is in the allowedOrigins array or if it’s undefined (for server-to-server requests)
+//       if (allowedOrigins.includes(origin) || !origin) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
 //   })
 // );
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.get("/api/", (req, res) => {
   res.send("Hello, World!");
@@ -132,58 +129,6 @@ app.patch("/api/orders/:id", async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 });
-
-// User Registration
-
-// app.post("/api/register", async (req, res) => {
-//   try {
-//     const { username, email, password } = req.body;
-
-//     const existingUser = await User.findOne({ email });
-
-//     if (existingUser) {
-//       return res.status(400).json({ message: "User already exists" });
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 12);
-
-//     const newUser = new User({ username, email, password: hashedPassword });
-//     await newUser.save();
-
-//     res.status(201).json(newUser);
-//   } catch (error) {
-//     res.status(500).json({ message: "Server error" });
-//   }
-// });
-
-// // User Login
-
-// app.post("/api/login", async (req, res) => {
-//   const { email, password } = req.body;
-
-//   const user = await User.findOne({ email });
-
-//   if (!user) return res.status(400).json({ message: "User not found" });
-
-//   const isPasswordValid = await bcrypt.compare(password, user.password);
-
-//   if (!isPasswordValid)
-//     return res.status(400).json({ message: "Invalid password" });
-
-//   const token = jwt.sign({ id: user._id, email: user.email }, "Tushar@0945", {
-//     expiresIn: "10d",
-//   });
-
-//   res
-//     .cookie("access-token", token, {
-//       httpOnly: true,
-//       maxAge: 86400000,
-//     })
-//     .status(200)
-//     .json(user);
-// });
-
-// app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
