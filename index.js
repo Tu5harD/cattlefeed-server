19cc6e9607716134040c1b0114d3430c95f4cdae
DevTusharDukane <tushardukane9@gmail.com>
2024-11-11 17:52:09 +0530

    localhot cros added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://tushartraders.shop/",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
