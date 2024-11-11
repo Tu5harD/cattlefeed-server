453e1ab6de97d6836697c82af808aac3f6f706a3
DevTusharDukane <tushardukane9@gmail.com>
2024-11-11 17:45:24 +0530

    cros change

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tushartraders.shop/",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "10mb" }));
 app.get("/api/", (req, res) => {
