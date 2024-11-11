14a730e710a29860415d40c13560f36f8abad42f
DevTusharDukane <tushardukane9@gmail.com>
2024-11-11 23:14:11 +0530

    remove auth

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ const allowedOrigins = ["https://tushartraders.shop", "http://localhost:5173"];
 //   })
 // );
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.get("/api/", (req, res) => {
   res.send("Hello, World!");
