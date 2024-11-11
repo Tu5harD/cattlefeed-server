7000157b755464b0ad58343358bd83cc12ff1227
DevTusharDukane <tushardukane9@gmail.com>
2024-11-11 18:19:48 +0530

    set all origin

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://tushartraders.shop"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173", "https://tushartraders.shop"],
+//   })
+// );
+app.use(cors({ origin: "*" }));
 
 app.use(express.json({ limit: "10mb" }));
 app.get("/api/", (req, res) => {
