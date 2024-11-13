bfb4097ef0b229bc3ec34aac90a1918202a260f9
DevTusharDukane <tushardukane9@gmail.com>
2024-11-13 20:11:20 +0530

    extend limit

--- src/app.js
+++ src/app.js
@@ -9,13 +9,11 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "*",
+  origin: "https://www.tushartraders.shop",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   allowedHeaders: "Content-Type, Authorization",
-  credentials: true,
 };
 
 app.use(cors(corsOptions));
